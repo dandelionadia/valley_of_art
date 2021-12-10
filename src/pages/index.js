e2e4b6aeb76e4d08d28e825ff84702b03788d957
dandelionadia <nadia.ridko@gmail.com>
2021-12-10 21:39:38 +0100

    index: adds hero block

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,27 @@
 import * as React from "react";
 import { DefaultLayout } from "../components/DefaultLayout";
+import { Button } from "../components/Button";
+import { Container } from "../components/Container";
 
 const IndexPage = () => {
   return (
-    <main className="container m-auto">
+    <Container>
       <title>Portfolio page</title>
-      <DefaultLayout></DefaultLayout>
-    </main>
+      <DefaultLayout>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-32 my-56">
+          <div>left content</div>
+          <div className="grid">
+            <p className="text-3xl mb-5">Hi! Nice to meet you!</p>
+            <p className="text-violet-light mb-8">
+              My name is <span className="text-white">Nadiia</span> and I'm a
+              digital artist. Here I will have a short introductory text and
+              perhaps a few other things. Exciting!
+            </p>
+            <Button>Browse my work</Button>
+          </div>
+        </div>
+      </DefaultLayout>
+    </Container>
   );
 };
 
