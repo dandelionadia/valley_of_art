cbf0bdae35b5c9c676201269dff0b8a970f4790a
dandelionadia <nadia.ridko@gmail.com>
2021-12-12 14:52:48 +0100

    adds tailwindcss/aspect-ratio, image responsive component, fixes title font-weight

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 
 import { DefaultLayout } from "../components/DefaultLayout";
 import { Container } from "../components/Container";
+import Sofa from "../images/sofa_01.png";
 
 const IndexPage = () => {
   return (
@@ -12,7 +13,9 @@ const IndexPage = () => {
         <div className="grid lg:grid-cols-2 gap-x-32 my-56">
           <div className="mb-32 lg:mb-0">left content</div>
           <div>
-            <p className="text-3xl mb-5">Hi! Nice to meet you!</p>
+            <p className="text-3xl mb-5 font-extrabold">
+              Hi! Nice to meet you!
+            </p>
             <p className="text-violet-light mb-10">
               My name is <span className="text-white">Nadiia</span> and I'm a
               digital artist. Here I will have a short introductory text and
@@ -26,9 +29,9 @@ const IndexPage = () => {
             </Link>
           </div>
         </div>
-        <div className="my-56">
+        <div className="my-24 lg:my-56">
           <div className="grid mx-auto max-w-lg text-center mb-10">
-            <p className="text-3xl mb-5">My work</p>
+            <p className="text-3xl mb-5 font-extrabold">My work</p>
             <p className="text-violet-light">
               Here is a different subheading text explaining this section. Well,
               it can certainly take its time to make a point. It's okay.
@@ -57,10 +60,16 @@ const IndexPage = () => {
               </button>
             </Link>
           </div>
-          <div className="container grid md:grid-cols-2 lg:grid-cols-3 gap-10 bg-gray-900 p-10 rounded-2xl bg-opacity-50">
-            <div className="rounded-lg bg-white">1</div>
-            <div className="rounded-lg bg-white">2</div>
-            <div className="rounded-lg bg-white">3</div>
+          <div className="container grid md:grid-cols-2 lg:grid-cols-3 gap-10 bg-gray-900 p-10 rounded-2xl bg-opacity-40">
+            <div className="shadow-xl rounded-lg overflow-hidden aspect-w-1 aspect-h-1">
+              <img src={Sofa} className="object-cover" />
+            </div>
+            <div className="shadow-xl rounded-lg overflow-hidden aspect-w-1 aspect-h-1">
+              <img src={Sofa} className="object-cover" />
+            </div>
+            <div className="shadow-xl rounded-lg overflow-hidden aspect-w-1 aspect-h-1">
+              <img src={Sofa} className="object-cover" />
+            </div>
           </div>
         </div>
       </DefaultLayout>
