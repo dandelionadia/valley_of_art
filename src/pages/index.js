0bf9ec212932587fb63e79b6ea59db65afbf8b99
dandelionadia <nadia.ridko@gmail.com>
2021-12-12 09:17:28 +0100

    index: removes unneeded properties

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react";
+import { Link } from "gatsby";
+
 import { DefaultLayout } from "../components/DefaultLayout";
-import { Button } from "../components/Button";
+import { MyLink } from "../components/MyLink";
 import { Container } from "../components/Container";
 
 const IndexPage = () => {
@@ -9,15 +11,15 @@ const IndexPage = () => {
       <title>Portfolio page</title>
       <DefaultLayout>
         <div className="grid lg:grid-cols-2 gap-x-32 my-56">
-          <div>left content</div>
-          <div className="grid">
+          <div className="mb-32 lg:mb-0">left content</div>
+          <div>
             <p className="text-3xl mb-5">Hi! Nice to meet you!</p>
-            <p className="text-violet-light mb-8">
+            <p className="text-violet-light mb-10">
               My name is <span className="text-white">Nadiia</span> and I'm a
               digital artist. Here I will have a short introductory text and
               perhaps a few other things. Exciting!
             </p>
-            <Button>Browse my work</Button>
+            <MyLink>Browse my work</MyLink>
           </div>
         </div>
       </DefaultLayout>
