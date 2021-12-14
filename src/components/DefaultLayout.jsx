fe851282520f1c44831b0b234ee8b9576531faca
dandelionadia <nadia.ridko@gmail.com>
2021-12-14 08:48:22 +0100

    Footer: adds footer and fixes logo padding in header on the sm

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 export function DefaultLayout({ children }) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <div className="w-full h-px border-gray-700 border-t inset-y-0 mb-5" />
+      <div className="text-violet-light text-opacity-60 text-xs mb-5">
+        © 2022 Nadiia Ridko. All rights reserved.
+      </div>
+      <div className="text-violet-light text-opacity-60 text-xs mb-5">
+        Made by Nadiia and Artem (You're always will have a special place in my
+        hart ❤️).
+      </div>
+    </div>
+  );
+};
